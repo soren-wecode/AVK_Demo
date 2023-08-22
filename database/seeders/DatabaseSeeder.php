d364e2caea6fe8bf9210681fcd932d3bf8be1bea
soren-wecode <soren@wecode.dk>
2023-08-22 14:28:50 +0200

    add more product options, seed users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Wecode',
+            'email' => 'soren@wecode.dk',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory(10)->create();
+
 
         $this->call(ProductsSeeder::class);
         $this->call(ProductOptionsSeeder::class);
