587007d59f1d04dfe01ec5e88fb9c2229cbba1ca
soren-wecode <soren@wecode.dk>
2023-08-22 14:30:38 +0200

    change user password

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$ZgIIsVxCoJKKPxmKQPyFF.9HrdeEeNYHuY.qSbVjz4yryF/f.GAKq', // password
             'remember_token' => Str::random(10),
         ];
     }
