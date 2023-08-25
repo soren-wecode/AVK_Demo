74690b596cc89e77c16a346f2311142bff4256c6
soren-wecode <soren@wecode.dk>
2023-08-25 14:16:13 +0200

    better spacing between products in cart

--- resources/js/Components/Cart/CartProduct.vue
+++ resources/js/Components/Cart/CartProduct.vue
@@ -2,14 +2,20 @@
     <tr class="border-t border-gray-300">
         <td class="py-3"><img :src="'product_images/' + product.product.image" class="w-auto h-8" /></td>
         <td class="text-avk-blue">
-            <div class="font-bold">{{ product.product.name }}</div>
-            <div class="text-sm">AVK no.: {{ product.ref_nr }}</div>
+            <div class=mr-3>
+                <div class="font-bold">{{ product.product.name }}</div>
+                <div class="text-sm">AVK no.: {{ product.ref_nr }}</div>
+            </div>
         </td>
         <td >
-            <NumberInput v-model="quantity" :min="1" :max="product.stock" class="w-24"/>
+            <NumberInput v-model="quantity" :min="1" :max="product.stock" class="w-24 mr-3"/>
         </td>
-        <td>{{ product.price.toLocaleString("en-UK", { style: "currency", currency: "GBP" }) }}</td>
-        <td class="font-bold">{{ totalPrice }}</td>
+        <td>
+            <div class=mr-3>{{ product.price.toLocaleString("en-UK", { style: "currency", currency: "GBP" }) }}</div>
+        </td>
+        <td class="font-bold">
+            <div class=mr-3>{{ totalPrice }}</div>
+        </td>
         <td class="cursor-pointer">
             <XMark size="24" @click="removeFromCart"/>
         </td>
